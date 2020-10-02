da59a847e60f9fd0032e6e74873654297e987981
Riko Ade Rinanda <richo4r.tgz@gmail.com>
2020-10-02 21:20:52 +0700

    Beranda update

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -28,7 +28,7 @@ namespace WebApp.Controllers
             var result = DeviceDetector.GetInfoFromUserAgent(userAgent);
             ViewBag.Device = result.Match.DeviceType.ToString();
             var model = new HomeModel();
-            if(ViewBag.Device == "smartphone")
+            if(ViewBag.Device != "")
             {
                 return await Task.Run(() => View(model));
             }
@@ -45,7 +45,24 @@ namespace WebApp.Controllers
             var result = DeviceDetector.GetInfoFromUserAgent(userAgent);
             ViewBag.Device = result.Match.DeviceType.ToString();
             var model = new HomeModel();
-            if (ViewBag.Device == "smartphone")
+            if (ViewBag.Device != "")
+            {
+                return await Task.Run(() => View(model));
+            }
+            else
+            {
+                return await Task.Run(() => View(model));
+            }
+        }
+        public async Task<IActionResult> Beranda()
+        {
+            DeviceDetector.SetVersionTruncation(VersionTruncation.VERSION_TRUNCATION_NONE);
+            BotParser botParser = new BotParser();
+            var userAgent = Request.Headers["User-Agent"];
+            var result = DeviceDetector.GetInfoFromUserAgent(userAgent);
+            ViewBag.Device = result.Match.DeviceType.ToString();
+            var model = new HomeModel();
+            if (ViewBag.Device != "")
             {
                 return await Task.Run(() => View(model));
             }
---
+++ WebApp/WebApp/Controllers/TopupController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp.Models;
+using DeviceDetectorNET;
+using DeviceDetectorNET.Parser;
+
+namespace WebApp.Controllers
+{
+    public class TopupController : Controller
+    {
+        private readonly ILogger<TopupController> _logger;
+        public TopupController(ILogger<TopupController> logger)
+        {
+            _logger = logger;
+        }
+
+        [NoDirectAccess]
+        public async Task<IActionResult> TopupBerasAwal(int id)
+        {
+            DeviceDetector.SetVersionTruncation(VersionTruncation.VERSION_TRUNCATION_NONE);
+            BotParser botParser = new BotParser();
+            var userAgent = Request.Headers["User-Agent"];
+            var result = DeviceDetector.GetInfoFromUserAgent(userAgent);
+            ViewBag.Device = result.Match.DeviceType.ToString();
+            var model = new HomeModel();
+            if (ViewBag.Device != "")
+            {
+                return await Task.Run(() => View(model));
+            }
+            else
+            {
+                return await Task.Run(() => View(model));
+            }
+        }
+    }
+}
